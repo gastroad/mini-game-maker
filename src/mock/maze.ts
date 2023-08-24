351dbcfbd5c12ad9400c474316b04b968319eb57
진현성 <gostroad37@gmail.com>
2023-08-24 17:14:25 +0900

    mock data 추가

---
+++ src/mock/maze.ts
@@ -0,0 +1,33 @@
+import { MazeDataType, MazeSizeType, MazeType, PointType } from "@type/maze"
+
+export const mockStart: PointType = { x: 0, y: 0 }
+export const mockEnd: PointType = { x: 4, y: 4 }
+export const mockMazeSize: MazeSizeType = { col: 5, row: 5 }
+export const mockResolvedPath: PointType[] = [
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+    { x: 2, y: 0 },
+    { x: 3, y: 0 },
+    { x: 4, y: 0 },
+    { x: 4, y: 1 },
+    { x: 4, y: 2 },
+    { x: 4, y: 3 },
+]
+export const mockMazeData: MazeDataType = [
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+]
+export const mockMaze: MazeType = {
+    id: 'test',
+    title: 'test',
+    name: 'test',
+    mazeSize: mockMazeSize,
+    start: mockStart,
+    end: mockEnd,
+    mazeData: mockMazeData,
+    resolvedPath: mockResolvedPath,
+}
+
