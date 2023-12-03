11a70fd2bb7ac2976d319da691ed5a9cd384a91e
진현성 <gostroad37@gmail.com>
2023-12-03 13:51:16 +0900

    update test config

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,20 @@
 import { test, expect } from '@playwright/test';
 
+const mockMazeData = {
+  title: "playwight e2e test title",
+  name: "playwight e2e test name",
+  mazeSize: {
+    "row": 10,
+    "col": 10
+  },
+}
+
 test('has title', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle(/maze-maker/);
 });
 
 test('test making maze', async ({ page }) => {
-  const mockMazeData = {
-    title: "playwight e2e test title",
-    name: "playwight e2e test name",
-    mazeSize: {
-      "row": 10,
-      "col": 10
-    },
-  }
   const mazeTitleInputElementSelector = '#body > div > div.maze-form > div:nth-child(1) > input'
   const mazeNameInputElementSelector = '#body > div > div.maze-form > div:nth-child(2) > input'
   const mazeColInputElementSelecttor = '#body > div > div.maze-form > div:nth-child(3) > div:nth-child(1) > input'
@@ -71,14 +72,6 @@ test('test making maze', async ({ page }) => {
 });
 
 test('test play maze', async ({ page }) => {
-  const mockMazeData = {
-    title: "playwight e2e test title",
-    name: "playwight e2e test name",
-    mazeSize: {
-      "row": 10,
-      "col": 10
-    },
-  }
   await page.goto('/');
 
   //root 페이지 진입 확인
@@ -94,7 +87,7 @@ test('test play maze', async ({ page }) => {
   // 마지막 map 진입
   await page.click(`#body > div > ul > li:last-child > a`)
 
-
+  // map 확인
   await expect(page).toHaveTitle(`${mockMazeData.title}-${mockMazeData.name}`);
   await expect(page.getByRole('heading', { name: mockMazeData.title })).toBeVisible();
   // await expect(page.getAttribute).toHaveCount(mockMazeData.mazeSize.col * mockMazeData.mazeSize.row)
@@ -113,6 +106,4 @@ test('test play maze', async ({ page }) => {
 
   //정답보기 버튼 클릭
   await page.getByRole('button', { name: "정답보기" }).click()
-
-
 })
