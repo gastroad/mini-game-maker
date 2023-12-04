ac599e4572f87db13c649f7cb94e40674c729468
진현성 <gostroad37@gmail.com>
2023-12-04 09:31:16 +0900

    update e2e test

--- Jest.test.js
+++ Jest.test.js
@@ -3,51 +3,114 @@ describe("maze-maker", () => {
         await page.goto('http://localhost:3000/');
     });
 
-    it('should display "maze-maker" text on page', async () => {
+    const mockMazeData = {
+        title: "puppeteer e2e test title",
+        name: "puppeteer e2e test name",
+        mazeSize: {
+            "row": 10,
+            "col": 10
+        },
+    }
 
-        await expect(page).toMatchTextContent("MazeMaker");
+    test('test making maze', async () => {
+        // root 페이지 진입 확인
+        await expect(page.$eval('h3', el => el.textContent)).resolves.toBe('MazeMaker');
+        await expect(page.title()).resolves.toMatch(/maze-maker/);
 
+        // Make 버튼 클릭
         await page.click('#body > div > a:nth-child(3)');
-
         await page.waitForNavigation({ waitUntil: 'load' });
-        await page.waitForSelector('#body > div > div.maze-form > div:nth-child(1) > input', 10000)
-        // 맵 이름 입력
-        await page.type('#body > div > div.maze-form > div:nth-child(1) > input', 'puppeteer test map name');
+        await page.waitForSelector('h3', 10000)
+        // Make 페이지 진입 확인
+        await expect(page.$eval('h3', el => el.textContent)).resolves.toBe('Maker');
 
-        // 제작자 이름 입력
-        await page.type('#body > div > div.maze-form > div:nth-child(2) > input', 'puppeteer test maker name');
 
-        // 시작지점 변경
+        // maze title 변경
+        await page.click('#body > div > div.maze-form > div:nth-child(1) > input');
+        await page.keyboard.type(mockMazeData.title);
+
+        // maze name 변경
+        await page.click('#body > div > div.maze-form > div:nth-child(2) > input');
+        await page.keyboard.type(mockMazeData.name);
+
+        // maze col 변경
+        await page.click('#body > div > div.maze-form > div:nth-child(3) > div:nth-child(1) > input');
+        await page.keyboard.press('Backspace');
+        await page.keyboard.type(String(mockMazeData.mazeSize.col));
+
+        // maze row 변경
+        await page.click('#body > div > div.maze-form > div:nth-child(3) > div:nth-child(2) > input');
+        await page.keyboard.press('Backspace');
+        await page.keyboard.type(String(mockMazeData.mazeSize.row));
+
+        // 시작 지점 변경
         await page.click('#body > div > div.maze-maker > div > div:nth-child(1) > div:nth-child(2)');
 
-        // 컨트롤러 벽으로 변경
+        // controller 벽 선택
         await page.click('#body > div > div.maker-maze-controller > div:nth-child(2) > button:nth-child(1)');
-
-        // 벽 2개 선택
         await page.click('#body > div > div.maze-maker > div > div:nth-child(1) > div:nth-child(3)');
         await page.click('#body > div > div.maze-maker > div > div:nth-child(3) > div:nth-child(1)');
 
-        // 컨트롤러 종료지점으로 변경
+        // controller 종료 지점 선택
         await page.click('#body > div > div.maker-maze-controller > div:nth-child(1) > button:nth-child(2)');
-
-        // 종료지점 변경
         await page.click('#body > div > div.maze-maker > div > div:nth-child(5) > div:nth-child(4)');
 
         // 제출 버튼 클릭
         await page.click('#body > div > div.maker-maze-controller > button');
 
-        // 최초 페이지 로딩 확인
-        await page.waitForSelector('#body > div > a:nth-child(2)', 10000)
+        await page.waitForNavigation({ waitUntil: 'load' });
+        await page.waitForSelector('h3', 10000)
+        // 메인 페이지 진입 확인
+        await expect(page.title()).resolves.toMatch(/maze-maker/);
+        await expect(page.$eval('h3', el => el.textContent)).resolves.toBe('MazeMaker');
+    });
 
-        await expect(page).toMatchTextContent('Maker')
+    test('test play maze', async () => {
+        // root 페이지 진입 확인
+        await expect(page.$eval('h3', el => el.textContent)).resolves.toBe('MazeMaker');
 
-        // play Button 클릭
+        // play 버튼 클릭
         await page.click('#body > div > a:nth-child(2)');
 
-        await page.waitForSelector('#body > div > ul > li:nth-child(1)', 10000)
-        await expect(page).toMatchTextContent('Maze List')
+        // maplist 페이지 진입 확인
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+        await expect(page.title()).resolves.toMatch(/maze-maker-maplist/);
 
-        await page.screenshot({ path: 'example.png' });
+        // 마지막 map 진입
+        await page.waitForSelector('.maze-list-item');
+        await page.click('#body > div > ul > li:last-child > a');
+
+        // map 확인
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+        await page.waitForSelector('.maze-board');
+
+        await expect(page.title()).resolves.toMatch(`${mockMazeData.title}-${mockMazeData.name}`);
+        await expect(page.$eval('h3', el => el.textContent)).resolves.toBe(mockMazeData.title);
+        // Arrow key 입력 및 점수 확인
+        await page.keyboard.press('ArrowDown');
+        await page.keyboard.press('ArrowRight');
+        await page.keyboard.press('ArrowRight');
+        await page.keyboard.press('ArrowRight');
+        await page.keyboard.press('ArrowDown');
+        await page.keyboard.press('ArrowDown');
+
+
+
+
+        await page.waitForSelector('#body > div > div.score-board > span');
+        let scoreText = await page.$eval('#body > div > div.score-board > span', el => el.textContent);
+        console.log(scoreText)
+        await expect(scoreText).toBe('score: -6');
+
+        // 골인
+        await page.keyboard.press('ArrowDown');
+        scoreText = await page.$eval('#body > div > div.score-board > span', el => el.textContent);
+
+        await expect(scoreText).toBe('score: 0');
+
+        // 정답보기 버튼 클릭
+        await page.click('#body > div > div.play-maze-controller > button:nth-child(2)');
     });
+
 });
 
